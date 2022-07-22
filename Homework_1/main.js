06b974335d974cf5f4d43c5d4a180b9400631bc3
KyryloPikhno <Mr.Good@i.ua>
2022-07-22 20:54:53 +0300

    Commit

---
+++ Homework_1/main.js
@@ -0,0 +1,71 @@
+let word1 = 'hello';
+const word2 = 'own';
+let word3 = 'com';
+const word4 = 'ua';
+let number1 = 1;
+const number2 = 10;
+let number3 = -900;
+const number4 = 123;
+let number5 = 3.14;
+const number6 = 2.7;
+let number7 = 16;
+const bull1 = true;
+let bull2 = false;
+
+console.log(word1, word2, word3, word4, number1, number2, number3, number4, number5, number6, number7, bull1, bull2);
+
+alert(word1);
+alert(word2);
+alert(word3);
+alert(word4);
+alert(number1);
+alert(number2);
+alert(number3);
+alert(number4);
+alert(number5);
+alert(number6);
+alert(number7);
+alert(bull1);
+alert(bull2);
+
+document.write(word1,', ');
+document.write(word2,', ');
+document.write(word3,', ');
+document.write(word4,', ');
+document.write(number1,', ');
+document.write(number2,', ');
+document.write(number3,', ');
+document.write(number4,', ');
+document.write(number5,', ');
+document.write(number6,', ');
+document.write(number7,', ');
+document.write(bull1,', ');
+document.write(bull2,'.');
+
+const firstName = 'Kyrylo';
+const middleName = 'Pikhno';
+const lastName = 'Valerievich';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let firstNamePerson = prompt('Hello:) What is your name?');
+console.log(firstNamePerson);
+let lastNamePerson = prompt('Ok. What is your last name?)');
+console.log(lastNamePerson);
+let yearsPerson = prompt('Goog! How old are you?)');
+console.log(yearsPerson);
+
+
+
+
+
+
