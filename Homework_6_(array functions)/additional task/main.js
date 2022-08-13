c63601851a588d38d0c142539d0a29737b450a3b
KyryloPikhno <Mr.Good@i.ua>
2022-08-14 01:05:51 +0300

    My commit

--- Homework_6_(array functions)/additional task/main.js
+++ Homework_6_(array functions)/additional task/main.js
@@ -12,7 +12,7 @@ let arrayOfNumbers = [2, 5, -3, 3, 4, -2, 10, 11, 12, -1, 0, 1, 7, 8, 9, 6, 14, 
 
 // let sortA = arrayOfNumbers.sort((a, b) => b - a);
 // console.log(sortA);
-
+//
 // let sortB = arrayOfNumbers.sort((a, b) => a - b);
 // console.log(sortB);
 
@@ -35,7 +35,42 @@ console.log(reduce);
 // b) Відсортувати в зворотньому порядку
 // c) Отримати в новому масиві тільки ті слова, довжина яких більша за 4 букви (filter)
 // d) Змінити кожне слово в масиві додавши на початок 'Sam says *ваше слово*'
-//
+
+let someStrings = ['number', 'arr', 'false', 'some word', 'map', 'let', 'mac', 'mony Mony', 'sum', 'red', 'fixed', 'console', 'result', 'run', 'code', 'math', 'to', '123', 'login'];
+
+let sortWord = someStrings.sort((word1, word2) =>{
+    if (word1 > word2) {
+        return 1;
+    }
+    if (word1 < word2) {
+        return -1;
+    }
+    if (word1 === word2) {
+        return 0;
+}
+});
+    console.log(sortWord);
+
+let sortWordRevers = someStrings.sort((word1, word2) =>{
+    if (word1 < word2) {
+        return 1;
+    }
+    if (word1 > word2) {
+        return -1;
+    }
+    if (word1 === word2) {
+        return 0;
+    }
+});
+console.log(sortWordRevers);
+
+let filterWord = someStrings.filter(word => word.length >= 4);
+
+console.log(filterWord);
+
+let mapOfWord = someStrings.map(value => value = 'Sam says ' + value);
+console.log(mapOfWord);
+
 // 3) Все робити тільки за допомогою методів масивів!
 //     Дано масив :
 //     const users = [
@@ -57,12 +92,55 @@ console.log(reduce);
 // та зберегти це в новий масив (первинний масив залишиться без змін)
 // d) відсортувати його за індентифікатором
 // e) Всі хто одружений мають попасти у новий масив та отрмати квартиру (reduce)
-//
-// ______________________________________________________________________________________________________________________________________________________
-// РОБОТА В АУДИТОРІЇ
-// ______________________________________________________________________________________________________________________________________________________
-//
-//
+
+const users = [
+        {name: 'vasya', age: 31, isMarried: false},
+        {name: 'petya', age: 30, isMarried: true},
+        {name: 'kolya', age: 29, isMarried: true},
+        {name: 'olya', age: 28, isMarried: false},
+        {name: 'max', age: 30, isMarried: true},
+        {name: 'anya', age: 31, isMarried: false},
+        {name: 'oleg', age: 28, isMarried: false},
+        {name: 'andrey', age: 29, isMarried: true},
+        {name: 'masha', age: 30, isMarried: true},
+        {name: 'olya', age: 31, isMarried: false},
+        {name: 'max', age: 31, isMarried: true}
+    ];
+
+let sortUser = users.sort((user1, user2) => user1.age - user2.age);
+
+console.log(sortUser);
+
+let sortUserRevers = users.sort((user1, user2) => user2.age - user1.age);
+
+console.log(sortUserRevers);
+
+let mapUser = users.map((user, index) => {
+    return {
+        name: user.name,
+        age: user.age,
+        isMarried: user.isMarried,
+        id: index + Math.random(),
+    };
+});
+console.log(mapUser);
+
+let sortOfUser = mapUser.sort((user1, user2) => user2.id - user1.id);
+
+console.log(sortOfUser);
+
+let reduceUser = mapUser.reduce( (accum , user) =>{
+    if(user.isMarried){
+        user.flat =true
+        accum.usersWithFlat.push(user);
+    } else{
+        accum.usersWithoutFlat.push(user);
+    }
+    return accum;
+},{usersWithFlat :[],usersWithoutFlat :[]});
+
+console.log(reduceUser);
+
 // const cars = [
 //     {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400},
 //     {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250},
@@ -83,17 +161,48 @@ console.log(reduce);
 //     - двигун більше 3х літрів
 // - двигун = 2л
 // - виробник мерс
-// - двигун більше 3х літрів + виробник мерседес
-// - двигун більше 3х літрів + виробник субару
+// - двигун більше 2х літрів + виробник мерседес
+// - двигун більше 2х літрів + виробник субару
 // - сили більше ніж 300
 // - сили більше ніж 300 + виробник субару
-// - мотор серіі ej
-// - сили більше ніж 300 + виробник субару + мотор серіі ej
-// - двигун меньше 3х літрів + виробник мерседес
-// - двигун більше 2л + сили більше 250
-// - сили більше 250  + виробник бмв
-//
-//
+
+const cars = [
+    {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400},
+    {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250},
+    {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300},
+    {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140},
+    {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200},
+    {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165},
+    {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120},
+    {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120},
+    {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350},
+    {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180},
+    {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180},
+    {producer:"mercedes",model: "e63",year:2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
+    {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230}
+ ];
+
+let filterVolume = cars.filter(car => car.volume > 3);
+console.log(filterVolume);
+
+let filterVolumeTwo = cars.filter(car => car.volume === 2);
+console.log(filterVolumeTwo);
+
+let producerCar = cars.filter(car => car.producer === 'mercedes');
+console.log(producerCar);
+
+let filterVolumeAndProducer = cars.filter(car => car.volume > 2 && car.producer === 'mercedes');
+console.log(filterVolumeAndProducer);
+
+let filterVolumeAndProducerTwo = cars.filter(car => car.volume > 2 && car.producer === 'subaru');
+console.log(filterVolumeAndProducerTwo);
+
+let power = cars.filter(car => car.power > 300);
+console.log(power);
+
+let powerAndProducer = cars.filter(car =>car.power > 300 && car.producer === 'subaru' )
+console.log(powerAndProducer);
+
 // - взять слдующий массив
 // const usersWithAddress = [
 //     {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
@@ -155,5 +264,138 @@ console.log(reduce);
 // Answer: MinIndex = 3, MaxIndex = 6.
 //
 //
-//
+// //
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
+
+
+
+
+
+// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // Каждый
+//
+// - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+//     let str = "HTML JavaScript PHP";
+// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+//
+// - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
+//
+// - Дано список імен.
+//     let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// let n1 = 'Harry Potter'
+// let n2 = 'Ron Whisley'
+// let n3 = 'Hermione Granger'
+//
+//     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
+//
+// - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+// - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
+// Протестувати на значеннях
+// someemail@gmail.com
+// someeMAIL@gmail.com
+// someeMAIL@i.ua
+// some.email@gmail.com
+//
+// Примітка
+// Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
+//
+// - є масивlet coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+//
+// відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+//
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+//
+// - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+//
+//
+// -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+// -знайти наібльшу книжку.
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор
+// - вісортувати книжки по кількості сторінок по зростанню
+//
+//
+//
