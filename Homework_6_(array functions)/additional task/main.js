30acb44e0557dedc08191d8996a7a7c5812e9bf2
KyryloPikhno <Mr.Good@i.ua>
2022-08-15 21:18:56 +0300

    My commit

--- Homework_6_(array functions)/additional task/main.js
+++ Homework_6_(array functions)/additional task/main.js
@@ -231,8 +231,33 @@ console.log(powerAndProducer);
 // -- Залишити лише тих, в кого парні номери будинків.
 // -- Порахувати загальний вік всіх людей. (reduce)
 // -- Ті, хто одружений і старий за 30 має отримати обєкти child і попасти в новий масив (reduce)
-//
-//
+
+const usersWithAddress = [
+    {id: 9, name: 'vasya', age: 31, isMarried: false, address: {city: 'Kyiv', street: 'Gongadze', number: 16}},
+    {id: 2, name: 'petya', age: 30, isMarried: true, address: {city: 'Rivne', street: 'Zelena', number: 1}},
+    {id: 4, name: 'kolya', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Pasichna', number: 121}},
+    {id: 3, name: 'olya', age: 28, isMarried: false, address: {city: 'Rivne', street: 'Shevchenko', number: 90}},
+    {id: 8, name: 'max', age: 30, isMarried: true, address: {city: 'Lviv', street: 'Kriva Lipa', number: 115}},
+    {id: 6, name: 'anya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+    {id: 10, name: 'oleg', age: 28, isMarried: false, address: {city: 'Kyiv', street: 'Centralna', number: 22}},
+    {id: 5, name: 'andrey', age: 29, isMarried: true, address: {city: 'Lviv', street: 'Gorodotska', number: 43}},
+    {id: 1, name: 'masha', age: 30, isMarried: true, address: {city: 'Kyiv', street: 'Peremogi', number: 12}},
+    {id: 7, name: 'olya', age: 31, isMarried: false, address: {city: 'Lviv', street: 'Naukova', number: 16}},
+    {id: 11, name: 'max', age: 31, isMarried: true, address: {city: 'Rivne', street: 'Ivana Franka', number: 121}}
+];
+
+let sortId = usersWithAddress.sort((user1, user2) => user1.id - user2.id);
+console.log(sortId);
+
+let sortIdRevers = usersWithAddress.sort((user1, user2) => user2.id - user1.id);
+console.log(sortIdRevers);
+
+
+
+
+
+
+
 // ______________________________________________________________________________________________________________________________________________________
 // Додатково
 // ______________________________________________________________________________________________________________________________________________________
