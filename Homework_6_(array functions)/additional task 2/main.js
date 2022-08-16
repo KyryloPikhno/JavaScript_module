d4b0e03f09abff8c346fd7eaf2e21c461a05ef21
KyryloPikhno <Mr.Good@i.ua>
2022-08-16 21:16:40 +0300

    My commit

--- Homework_6_(array functions)/additional task 2/main.js
+++ Homework_6_(array functions)/additional task 2/main.js
@@ -14,11 +14,10 @@ function cutString(str, n) {
 console.log(cutString('наслаждение', 3));
 
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
-
 // document.writeln(delete_characters(str, 7)); // Каждый
 
-function deleteCharacters(str, n){
-    return  str.split('').slice(0, n).join('');
+function deleteCharacters(str, n) {
+    return str.split('').slice(0, n).join('');
 }
 
 let str2 = 'Каждый охотник желает знать';
@@ -28,28 +27,29 @@ document.write(deleteCharacters(str2, 7));
 //     let str = "HTML JavaScript PHP";
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
 
-let str3 = "HTML JavaScript PHP";
+let strHTML = "HTML JavaScript PHP";
 let insertDash = str => {
     let newStr = str.replaceAll(' ', '-').toUpperCase();
     document.write(newStr);
 }
-insertDash(str3)
+insertDash(strHTML);
 
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
 
 let c = 'javascript is cool';
 let firstLetterToUpperCase = str =>{
     let firstLetter = str[0].toUpperCase();
-    let nextLetter = str.slice(1, str.length)
+    let nextLetter = str.slice(1, str.length);
     let concat = firstLetter.concat(nextLetter);
     document.write(concat);
 }
 firstLetterToUpperCase(c);
 
 // - Дано список імен.
-let n1 = 'Harry..Potter'
-let n2 = 'Ron---Whisley'
-let n3 = 'Hermione__Granger'
+
+let n1 = 'Harry..Potter';
+let n2 = 'Ron---Whisley';
+let n3 = 'Hermione__Granger';
 
 // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
 // let n1 = 'Harry Potter'
@@ -62,20 +62,21 @@ let validName = n => {
         .replaceAll('__', ' ')
     console.log(newName);
 }
+
 validName(n1);
 validName(n2);
 validName(n3);
 
 //     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
 
-let randomNumber = (arr,num) =>{
+let randomNumber = (arr, num) => {
     for (let i = 0; i < num; i++) {
-        arr.push(Math.round(Math.random()*100))
+        arr.push(Math.round(Math.random() * 100));
     }
-    console.log(arr)
-}
+    console.log(arr);
+};
 let array = [];
-randomNumber(array,15)
+randomNumber(array, 15);
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
 
@@ -109,6 +110,9 @@ capitalize(c)
 
 // - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
 // Протестувати на значеннях
+
+
+
 
 // Примітка
 // Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
