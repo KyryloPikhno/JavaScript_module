44fa8da7eb41f773a136b5343933bfc4d94ff41f
KyryloPikhno <Mr.Good@i.ua>
2025-07-14 12:00:00 -0600

    Add algorithm categories for next steps

--- algorithms/two-pointers/main.js
+++ algorithms/two-pointers/main.js
@@ -128,3 +128,12 @@ function hasPairSum(arr, target) {
 }
 
 console.log(hasPairSum([1, 2, 4, 6, 10], 8))
+
+// 1	Two Pointers
+// 2	Sliding Window
+// 3	HashMap / Set
+// 4	Prefix Sum + Binary Search
+// 5	Stack / Queue
+// 6	DFS / BFS
+// 7	Sorting + Greedy
+// 8	Dynamic Programming (DP)
