5901f9ce9be145e03f91955b0e79b6b7950d1f49
KyryloPikhno <Mr.Good@i.ua>
2025-09-09 14:16:27 -0600

    Add Queue class implementation with enqueue, dequeue, front, and isEmpty methods, along with example usage

---
+++ algorithms/basics/data-structures/core/queue.js
@@ -0,0 +1,30 @@
+class Queue {
+    constructor() {
+      this.items = [];
+    }
+  
+    enqueue(element) {
+      this.items.push(element); 
+    }
+  
+    dequeue() {
+      return this.items.shift(); 
+    }
+  
+    front() {
+      return this.items[0]; 
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  }
+  
+  const q = new Queue();
+  q.enqueue("A");
+  q.enqueue("B");
+  q.enqueue("C");
+  
+  console.log(q.dequeue()); // A
+  console.log(q.front());   // B
+  
